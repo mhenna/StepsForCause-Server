5fa4d2afb1089b85f3e535a752edc1c7417c94a0
mhenna <mhenna@aucegypt.edu>
2020-01-22 20:16:22 +0200

    fixed test for registration

--- src/test/java/com/techdev/stepsforcause/StepsforcauseApplicationTests.java
+++ src/test/java/com/techdev/stepsforcause/StepsforcauseApplicationTests.java
@@ -1,11 +1,17 @@
 package com.techdev.stepsforcause;
 
+import com.mongodb.Mongo;
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoClients;
 import com.techdev.stepsforcause.controller.UserController;
 import com.techdev.stepsforcause.models.User;
 import com.techdev.stepsforcause.routes.Routes;
 import com.techdev.stepsforcause.utils.HelperFuncs;
 import com.techdev.stepsforcause.utils.JwtToken;
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.MockitoAnnotations;
@@ -20,6 +26,9 @@ import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+
+import java.net.URI;
+import java.net.URISyntaxException;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.hasValue;
@@ -42,9 +51,6 @@ class StepsforcauseApplicationTests {
     @Autowired
     private JwtToken jwtToken;
 
-    @MockBean
-    HelperFuncs helperFuncs;
-
     @Autowired
     private UserController userController;
 
@@ -55,7 +61,6 @@ class StepsforcauseApplicationTests {
     void setupDB() {
         MockitoAnnotations.initMocks(this);
         mongoTemplate.dropCollection(User.class);
-        mongoTemplate.indexOps(User.class).ensureIndex(new Index().on("email", Sort.Direction.ASC).unique());
         mongoTemplate.save(youssef);
         mongoTemplate.save(mostafa);
     }
@@ -86,70 +91,6 @@ class StepsforcauseApplicationTests {
 
         mockMvc.perform(get("/" + Routes.USERS))
                 .andExpect(status().isUnauthorized());
-    }
-
-    /* This is the part where registration endpoint is tested with all scenarios
-        1. Register correctly
-        2. Register with no first name
-        3. Register with no last name
-        4. Register with no password
-        5. Register with an incorrectly formatted email
-        6. Register again to ensure duplicate key error returns
-     */
-    @Test
-    public void testRegister() throws Exception {
-        doNothing().when(helperFuncs).sendEmail(anyString(), anyString(), anyString(), anyString());
-
-        User u = new User("karim", "mady", "karim@karim.com", "hello", "");
-        mockMvc.perform(post("/" + Routes.USERS)
-                .contentType(MediaType.APPLICATION_JSON)
-                .content("{\"firstName\": \"karim\", \"lastName\": \"mady\", \"email\":\"karim@karim.com\", \"password\": \"hello\"}")
-                .accept(MediaType.APPLICATION_JSON))
-                    .andExpect(status().isOk())
-                    .andExpect(jsonPath("$.user", hasValue(u.firstName)))
-                    .andExpect(jsonPath("$.user", hasValue(u.lastName)))
-                    .andExpect(jsonPath("$.user", hasValue(u.email)))
-                    .andExpect(jsonPath("$.user", hasValue(u.password)));
-
-        Query q = new Query(Criteria.where("email").is(u.email));
-        User retrievedUser = mongoTemplate.findOne(q, User.class);
-
-        Assert.assertNotNull(retrievedUser);
-        Assert.assertEquals("karim", retrievedUser.firstName);
-        Assert.assertEquals("mady", retrievedUser.lastName);
-        Assert.assertEquals("karim@karim.com", retrievedUser.email);
-
-        mockMvc.perform(post("/" + Routes.USERS)
-                .contentType(MediaType.APPLICATION_JSON)
-                .content("{\"firstName\": \"\", \"lastName\": \"mady\", \"email\":\"karim@karim.com\", \"password\": \"hello\"}")
-                .accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isBadRequest());
-
-        mockMvc.perform(post("/" + Routes.USERS)
-                .contentType(MediaType.APPLICATION_JSON)
-                .content("{\"firstName\": \"karim\", \"lastName\": \"\", \"email\":\"karim@karim.com\", \"password\": \"hello\"}")
-                .accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isBadRequest());
-
-        mockMvc.perform(post("/" + Routes.USERS)
-                .contentType(MediaType.APPLICATION_JSON)
-                .content("{\"firstName\": \"karim\", \"lastName\": \"mady\", \"email\":\"karim@karim.com\", \"password\": \"\"}")
-                .accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isBadRequest());
-
-        mockMvc.perform(post("/" + Routes.USERS)
-                .contentType(MediaType.APPLICATION_JSON)
-                .content("{\"firstName\": \"karim\", \"lastName\": \"mady\", \"email\":\"karimkarim.com\", \"password\": \"hello\"}")
-                .accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isBadRequest());
-
-        mockMvc.perform(post("/" + Routes.USERS)
-                .contentType(MediaType.APPLICATION_JSON)
-                .content("{\"firstName\": \"karim\", \"lastName\": \"mady\", \"email\":\"karim@karim.com\", \"password\": \"hello\"}")
-                .accept(MediaType.APPLICATION_JSON))
-                    .andExpect(status().isBadRequest());
-
-        mongoTemplate.remove(retrievedUser);
     }
 
     /* This is the part where verification along with login endpoints are tested
@@ -232,5 +173,106 @@ class StepsforcauseApplicationTests {
                 .content("{\"email\": \"youssef@emc.com\", \"stepCount\": \"2000\"}")
                 .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isUnauthorized());
+    }
+
+    @AfterAll
+    public static void postTests() {
+        MongoClient mongoClient = new MongoClient( "localhost" , 27017 );
+        mongoClient.getDatabase("steps-for-cause-test").getCollection("user").drop();
+    }
+}
+
+@SpringBootTest(properties = {"spring.data.mongodb.database=steps-for-cause-test", "spring.data.mongodb.uri=mongodb://localhost:27017"})
+@AutoConfigureMockMvc
+class RegistrationTests {
+    @MockBean
+    private HelperFuncs helperFuncs;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
+    private User youssef = new User("youssef", "elhady", "youssef@emc.com", "hello", "hi");
+    private User mostafa = new User("mostafa", "henna", "mostafa@emc.com", "hello", "");
+
+    @BeforeEach
+    void setupDB() {
+        MockitoAnnotations.initMocks(this);
+        mongoTemplate.dropCollection(User.class);
+        mongoTemplate.indexOps(User.class).ensureIndex(new Index().on("email", Sort.Direction.ASC).unique());
+        mongoTemplate.save(youssef);
+        mongoTemplate.save(mostafa);
+    }
+
+    /* This is the part where registration endpoint is tested with all scenarios
+        1. Register correctly
+        2. Register with no first name
+        3. Register with no last name
+        4. Register with no password
+        5. Register with an incorrectly formatted email
+        6. Register again to ensure duplicate key error returns
+     */
+    @Test
+    public void testRegister() throws Exception {
+        doNothing().when(helperFuncs).sendEmail(anyString(), anyString(), anyString(), anyString());
+
+        User u = new User("karim", "mady", "karim@karim.com", "hello", "");
+        mockMvc.perform(post("/" + Routes.USERS)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\"firstName\": \"karim\", \"lastName\": \"mady\", \"email\":\"karim@karim.com\", \"password\": \"hello\"}")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.user", hasValue(u.firstName)))
+                .andExpect(jsonPath("$.user", hasValue(u.lastName)))
+                .andExpect(jsonPath("$.user", hasValue(u.email)))
+                .andExpect(jsonPath("$.user", hasValue(u.password)));
+
+        Query q = new Query(Criteria.where("email").is(u.email));
+        User retrievedUser = mongoTemplate.findOne(q, User.class);
+
+        Assert.assertNotNull(retrievedUser);
+        Assert.assertEquals("karim", retrievedUser.firstName);
+        Assert.assertEquals("mady", retrievedUser.lastName);
+        Assert.assertEquals("karim@karim.com", retrievedUser.email);
+
+        mockMvc.perform(post("/" + Routes.USERS)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\"firstName\": \"\", \"lastName\": \"mady\", \"email\":\"karim@karim.com\", \"password\": \"hello\"}")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest());
+
+        mockMvc.perform(post("/" + Routes.USERS)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\"firstName\": \"karim\", \"lastName\": \"\", \"email\":\"karim@karim.com\", \"password\": \"hello\"}")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest());
+
+        mockMvc.perform(post("/" + Routes.USERS)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\"firstName\": \"karim\", \"lastName\": \"mady\", \"email\":\"karim@karim.com\", \"password\": \"\"}")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest());
+
+        mockMvc.perform(post("/" + Routes.USERS)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\"firstName\": \"karim\", \"lastName\": \"mady\", \"email\":\"karimkarim.com\", \"password\": \"hello\"}")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest());
+
+        mockMvc.perform(post("/" + Routes.USERS)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\"firstName\": \"karim\", \"lastName\": \"mady\", \"email\":\"karim@karim.com\", \"password\": \"hello\"}")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest());
+
+        mongoTemplate.remove(retrievedUser);
+    }
+
+    @AfterAll
+    public static void postTests() {
+        MongoClient mongoClient = new MongoClient( "localhost" , 27017 );
+        mongoClient.getDatabase("steps-for-cause-test").getCollection("user").drop();
     }
 }
